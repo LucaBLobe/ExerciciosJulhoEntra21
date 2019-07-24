f970a492340b49292a41026633799bf171e8176c
LucaBLobe <lucablobe@gmail.com>
2019-07-23 23:12:47 -0300

    Exercícios 27-29

--- ExerciciosJulho/src/br/com/orientacao_a_objetos/Agencia.java
+++ ExerciciosJulho/src/br/com/orientacao_a_objetos/Agencia.java
@@ -3,5 +3,9 @@ package br.com.orientacao_a_objetos;
 public class Agencia {
 	
 	public int numeroAgencia;
+	public Agencia {
+		this.numeroAgencia = numeroAgencia;
+		
+	}
 
 }
---
+++ ExerciciosJulho/src/br/com/testes_orientacao_a_objetos/TesteGerente.java
@@ -0,0 +1,20 @@
+package br.com.testes_orientacao_a_objetos;
+
+import br.com.orientacao_a_objetos.Gerente;
+
+public class TesteGerente {
+
+	public static void main(String[] args) {
+		
+		Gerente g1 =  new Gerente();
+		g1.nome = "Luandrea";
+		g1.salario = 1500.0;
+		System.out.println("salario: "+g1.salario);
+		g1.aumentarSalario();
+		System.out.println("salario: "+g1.salario);
+		g1.aumentarSalario(25);
+		System.out.println("salario: "+g1.salario);
+
+	}
+
+}
