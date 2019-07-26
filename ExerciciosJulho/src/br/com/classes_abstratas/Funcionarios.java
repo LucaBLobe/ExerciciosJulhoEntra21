5018b4bc8f73d14daffd09846e3ff31078040b60
LucaBLobe <lucablobe@gmail.com>
2019-07-25 23:21:29 -0300

    Exercício 91-93

---
+++ ExerciciosJulho/src/br/com/classes_abstratas/Funcionarios.java
@@ -0,0 +1,23 @@
+package br.com.classes_abstratas;
+
+public abstract class Funcionarios {
+	
+	private String nome;
+	private int codigo;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	
+
+}
---
+++ ExerciciosJulho/src/br/com/classes_abstratas/TestaFuncionarios.java
@@ -0,0 +1,15 @@
+package br.com.classes_abstratas;
+
+public class TestaFuncionarios {
+	
+	public static void main(String[] args) {
+		Funcionarios fun = new Funcionarios();
+		
+		fun.setNome("Luca");
+		fun.setCodigo(150);
+
+		
+	}
+	
+
+}
