e48f1994d5aba2a83b65e0d92536ce81c914ca99
LucaBLobe <lucablobe@gmail.com>
2019-07-25 22:44:53 -0300

    Exercício 82

--- ExerciciosJulho/src/br/com/classes_abstratas/TestaConta.java
+++ ExerciciosJulho/src/br/com/classes_abstratas/TestaConta.java
@@ -1,13 +1,12 @@
 package br.com.classes_abstratas;
 
-import br.com.orientacao_a_objetos.Conta;
+import br.com.classes_abstratas.Conta;;
 
 public class TestaConta {
 	
 	public static void main(String[] args) {
 		
-		Conta c1 = new Conta(null);
-		Conta c2 = new Conta(null);
+		Conta c1 = new Conta();
 		
 		c1.setNumero(10001);
 		c1.setSaldo(1000);
@@ -15,7 +14,7 @@ public class TestaConta {
 
 		
 		System.out.println("Numero: " + c1.getNumero());
-		System.out.print("saldo: "+c1.mostrarSaldo()+"\n");
+		System.out.print("saldo: "+c1.getSaldo()+"\n");
 		System.out.println("limite: " + c1.getLimite());
 		
 		
